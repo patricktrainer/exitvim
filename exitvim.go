0bb8a67c913564a5bd626df23cccbf7a468c723b
Patrick Trainer <patrick.trainer@socure.com>
2023-12-22 21:48:51 -0600

    Refactor process handling and add user selection

--- exitvim.go
+++ exitvim.go
@@ -3,37 +3,81 @@ package main
 import (
 	"log"
 	"os"
-	"os/exec"
 	"strconv"
 	"strings"
 	"syscall"
+	"bufio"
+	"fmt"
+
+	"github.com/shirou/gopsutil/process"
 )
 
-// findProcessPIDs finds PIDs of processes with the given name.
-func findProcessPIDs(processName string) ([]int, error) {
-	var pids []int
-	cmd := exec.Command("pgrep", processName)
-	output, err := cmd.Output()
+type ProcessInfo struct {
+	PID int32
+	Name string
+	CPU float64
+	Memory float32
+}
+
+// listAllProcesses lists all running processes with extended information.
+func listAllProcesses() ([]ProcessInfo, error) {
+	var processesInfo []ProcessInfo
+
+	processes, err := process.Processes()
 	if err != nil {
 		return nil, err
 	}
 
-	lines := strings.Split(strings.TrimSpace(string(output)), "\n")
-	for _, line := range lines {
-		pid, err := strconv.Atoi(line)
+	for _, p := range processes {
+		name, err := p.Name()
 		if err != nil {
-			log.Printf("Error converting PID from string: %v", err)
-			continue // Skip this line and continue with the next
+			continue
 		}
-		pids = append(pids, pid)
+
+		cpu, err := p.CPUPercent()
+		if err != nil {
+			continue
+		}
+
+		memInfo, err := p.MemoryInfo()
+		if err != nil {
+			continue
+		}
+
+		processesInfo = append(processesInfo, ProcessInfo{
+			PID: p.Pid,
+			Name: name,
+			CPU: cpu,
+			Memory: float32(memInfo.RSS) / 1024 / 1024, // Convert to MB
+		})
 	}
 
-	return pids, nil
+	return processesInfo, nil
 }
+
+// userSelectProcess allows the user to select a process from the list.
+func userSelectProcess(processes []ProcessInfo) (int32, error) {
+	fmt.Println("Select a process to send signal to:")
+	for _, p := range processes {
+		fmt.Printf("PID: %d, Name: %s, CPU: %.2f%%, Memory: %.2f MB\n", p.PID, p.Name, p.CPU, p.Memory)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter PID: ")
+	pidStr, _ := reader.ReadString('\n')
+	pidStr = strings.TrimSpace(pidStr)
+	pid, err := strconv.ParseInt(pidStr, 10, 32)
+	if err != nil {
+		return 0, err
+	}
+
+	return int32(pid), nil
+}
+
 
 // sendSignalToProcess sends a specific signal to a process.
 func sendSignalToProcess(pid int, sig syscall.Signal) error {
-	process, err := os.FindProcess(pid)
+	process, err := os.FindProcess(int(pid))
 	if err != nil {
 		return err
 	}
@@ -41,25 +85,20 @@ func sendSignalToProcess(pid int, sig syscall.Signal) error {
 }
 
 func main() {
-	processName := "vim"
-
-	pids, err := findProcessPIDs(processName)
+	processes, err := listAllProcesses()
 	if err != nil {
-		log.Fatalf("Failed to find PIDs for %s: %v", processName, err)
+		log.Fatalf("Failed to list processes: %v", err)
 	}
 
-	for _, pid := range pids {
-		err = sendSignalToProcess(pid, syscall.SIGTERM)
-		if err != nil {
-			log.Printf("Failed to send SIGTERM to %s process %d: %v", processName, pid, err)
-			continue
-		}
-		log.Printf("Sent SIGTERM to %s process with PID %d", processName, pid)
+	selectedPID, err := userSelectProcess(processes)
+	if err != nil {
+		log.Fatalf("Failed to select process: %v", err)
 	}
 
-	if len(pids) > 0 {
-		log.Printf("Processed %d %s processes", len(pids), processName)
+	err = sendSignalToProcess(int(selectedPID), syscall.SIGTERM)
+	if err != nil {
+		log.Printf("Failed to send SIGTERM to process %d: %v", selectedPID, err)
 	} else {
-		log.Printf("No %s processes found", processName)
+		log.Printf("Sent SIGTERM to process with PID %d", selectedPID)
 	}
 }
